5f12f09ad4f9403d7e1fbea30db67caedf89319a
Joshua Chapman <joshua.chapman@joshuachp.dev>
2023-03-09 12:09:49 +0100

    feat: add clap for cli and completion

---
+++ src/cli.rs
@@ -0,0 +1,26 @@
+use std::io;
+
+use clap::{CommandFactory, Parser, Subcommand};
+use clap_complete::{generate, Shell};
+
+#[derive(Parser)]
+#[command(version, about, long_about)]
+pub struct Cli {
+    #[clap(subcommand)]
+    pub subcommand: Command,
+}
+
+impl Cli {
+    pub fn generate_completion(shell: Shell) {
+        generate(shell, &mut Cli::command(), "dockers", &mut io::stdout())
+    }
+}
+
+#[derive(Subcommand)]
+pub enum Command {
+    Run,
+    /// Generate shell completion for a given shell
+    Completion {
+        shell: Shell,
+    },
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::Parser;
+use cli::Cli;
+
+pub mod cli;
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.subcommand {
+        cli::Command::Run => println!("Run"),
+        cli::Command::Completion { shell } => Cli::generate_completion(shell),
+    }
 }
