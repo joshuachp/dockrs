3632128a3365e52175fb927fd755bf787f7c6805
Joshua Chapman <joshua.chapman@joshuachp.dev>
2023-03-09 12:26:31 +0100

    feat: add tracing and color-eyre for better error reporting

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 use clap::Parser;
 use cli::Cli;
+use color_eyre::Result;
+use tracing_subscriber::{prelude::*, EnvFilter};
 
 pub mod cli;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
+
     let cli = Cli::parse();
 
     match cli.subcommand {
         cli::Command::Run => println!("Run"),
         cli::Command::Completion { shell } => Cli::generate_completion(shell),
     }
+
+    Ok(())
 }
 
 #[cfg(test)]
