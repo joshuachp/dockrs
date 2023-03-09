2f0b46b64e453fc46298139e0978144f27137740
Joshua Chapman <joshua.chapman@joshuachp.dev>
2023-03-09 11:42:25 +0100

    feat: initial project bootstrap

---
+++ flake.nix
@@ -0,0 +1,89 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    naersk = {
+      url = "github:nmattia/naersk/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , naersk
+    , fenix
+    , ...
+    }:
+    let
+      supportedSystems = with flake-utils.lib.system; [
+        x86_64-linux
+        x86_64-darwin
+        aarch64-linux
+        aarch64-darwin
+      ];
+      eachSystemMap = flake-utils.lib.eachSystemMap supportedSystems;
+    in
+    rec {
+      packages = eachSystemMap (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          naersk-lib = naersk.lib.${system};
+          fenix-pkg = fenix.packages.${system}.stable;
+        in
+        {
+          default =
+            (naersk-lib.override {
+              inherit (fenix-pkg) cargo rustc;
+            }).buildPackage {
+              nativeBuildInputs = with pkgs; [ installShellFiles ];
+              root = ./.;
+              overrideMain = _: {
+                postInstall = ''
+                  installShellCompletion --cmd note \
+                    --bash <($out/bin/dockrs completion bash) \
+                    --fish <($out/bin/dockrs completion fish) \
+                    --zsh <($out/bin/dockrs completion zsh)
+                '';
+              };
+            };
+        });
+
+      apps = eachSystemMap (system: {
+        default = flake-utils.lib.mkApp {
+          drv = packages.${system}.default;
+        };
+      });
+
+      devShells = eachSystemMap (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          fenix-pkg = fenix.packages.${system}.stable;
+        in
+        {
+          default =
+            pkgs.mkShell
+              {
+                buildInputs = with pkgs; [
+                  (fenix-pkg.withComponents [
+                    "cargo"
+                    "clippy"
+                    "rust-src"
+                    "rustc"
+                    "rustfmt"
+                  ])
+                  rust-analyzer
+                  pre-commit
+
+                  cargo-edit
+                ];
+              };
+        });
+    };
+}
