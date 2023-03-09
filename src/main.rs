82568cb4abccd22c90dfd4dc8b43357e34a47ecf
Joshua Chapman <joshua.chapman@joshuachp.dev>
2023-03-09 12:09:49 +0100

    feat: add clap for cli and completion

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
+use clap::Parser;
+use cli::Cli;
+
+pub mod cli;
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.subcommand {
+        cli::Command::Run => println!("Run"),
+        cli::Command::Completion { shell } => Cli::generate_completion(shell),
+    }
 }
 
 #[cfg(test)]
