4a1c7ba552c00126e7f85f2a5b9915df475c9fc3
Joshua Chapman <joshua.chapman@joshuachp.dev>
2023-03-13 12:54:24 +0100

    feat

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world! {} + {} = {}", 1, 2, add(1, 2));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
 }
